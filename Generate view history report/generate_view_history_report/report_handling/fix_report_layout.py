e2dec7ef98f6c1120326db9143e3a390b5583ea9
Manuel Galli <gmanuel89@gmail.com>
2025-03-19 15:02:19 +0100

    fixed output report layout

--- Generate view history report/generate_view_history_report/report_handling/fix_report_layout.py
+++ Generate view history report/generate_view_history_report/report_handling/fix_report_layout.py
@@ -1,33 +1,39 @@
 #####
 # Author: Manuel Galli
 # e-mail: gmanuel89@gmail.com
-# Updated date: 2024-10-21
+# Updated date: 2025-03-19
 #####
 
 ## Import functions and constants
 from report_handling.rename_csv_headers import rename_csv_headers
 from constants import *
 
-## Fix the layout of the report
+## Fix the layout of the output report
 def fix_report_layout(report_content: list[dict]) -> list[dict]:
     # Rename and order headers
-    csv_header_renamed_and_ordered = {'title': OUTPUT_REPORT_COLUMN_TITLE + ' (english)',
-                                      'year': OUTPUT_REPORT_COLUMN_YEAR,
-                                      'type': OUTPUT_REPORT_COLUMN_TYPE,
-                                      'traktId': OUTPUT_REPORT_COLUMN_TRAKT_ID,
-                                      'imdbId': OUTPUT_REPORT_COLUMN_IMDB_ID,
-                                      'latestWatchedEpisode': OUTPUT_REPORT_COLUMN_LAST_WATCHED_EPISODE,
-                                      'watchedEpisodes': OUTPUT_REPORT_COLUMN_WATCHED_EPISODES,
-                                      'percentageOfCompletion': OUTPUT_REPORT_COLUMN_PERCENTAGE_OF_COMPLETION,
-                                      'showStatus': OUTPUT_REPORT_COLUMN_STATUS,
-                                      'alias (it)': OUTPUT_REPORT_COLUMN_TITLE + ' (italian)',
-                                      'listedAt': OUTPUT_REPORT_COLUMN_LISTED_AT,
-                                      'season': OUTPUT_REPORT_COLUMN_SEASON,
-                                      'episodeNumber': OUTPUT_REPORT_COLUMN_EPISODE_NUMBER,
-                                      'episodeTitle': OUTPUT_REPORT_COLUMN_EPISODE_TITLE,
-                                      'episodeWatchedAt': OUTPUT_REPORT_COLUMN_WATCHED_AT,
-                                      'movieWatchedAt': OUTPUT_REPORT_COLUMN_WATCHED_AT
+    csv_header_renamed_and_ordered = {
+        'originalTitle': OUTPUT_REPORT_COLUMN_TITLE + ' (original)',
+        'title': OUTPUT_REPORT_COLUMN_TITLE + ' (international english)',
+        'year': OUTPUT_REPORT_COLUMN_YEAR,
+        'type': OUTPUT_REPORT_COLUMN_TYPE,
+        'traktId': OUTPUT_REPORT_COLUMN_TRAKT_ID,
+        'imdbId': OUTPUT_REPORT_COLUMN_IMDB_ID,
+        'latestWatchedEpisode': OUTPUT_REPORT_COLUMN_LAST_WATCHED_EPISODE,
+        'watchedEpisodes': OUTPUT_REPORT_COLUMN_WATCHED_EPISODES,
+        'percentageOfCompletion': OUTPUT_REPORT_COLUMN_PERCENTAGE_OF_COMPLETION,
+        'showStatus': OUTPUT_REPORT_COLUMN_STATUS,
+        'listedAt': OUTPUT_REPORT_COLUMN_LISTED_AT,
+        'season': OUTPUT_REPORT_COLUMN_SEASON,
+        'episodeNumber': OUTPUT_REPORT_COLUMN_EPISODE_NUMBER,
+        'episodeTitle': OUTPUT_REPORT_COLUMN_EPISODE_TITLE,
+        'episodeWatchedAt': OUTPUT_REPORT_COLUMN_WATCHED_AT,
+        'movieWatchedAt': OUTPUT_REPORT_COLUMN_WATCHED_AT
     }
+    # Dynamic alias
+    for hd in report_content[0].keys():
+        if 'alias' in str(hd).lower():
+            csv_header_renamed_and_ordered[hd] = OUTPUT_REPORT_COLUMN_TITLE + str(hd).split('alias')[1]
+    # Remap headers
     report_content = rename_csv_headers(report_content, csv_header_renamed_and_ordered)
     # Return
     return report_content
